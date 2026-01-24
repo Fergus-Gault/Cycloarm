b0608466e391af0dc1af31172ed746fed7f0e337
Fergus Gault <fergus@gault.com>
2026-01-24 22:41:33 +0000

    Setup basic project structure

---
+++ firmware/src/board_a/pinmap.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+#include <Arduino.h>
+
+constexpr uint8_t EN_PIN = 8;
+
+constexpr uint8_t STEP_PINS[3] = {2,  // Base yaw
+                                  3,  // Shoulder
+                                  4}; // Elbow
+
+constexpr uint8_t DIR_PINS[3] = {5,  // Base yaw
+                                 6,  // Shoulder
+                                 7}; // Elbow
+
+constexpr int8_t ENDSTOP_PINS[3] = {-1,  // Base yaw (no endstop)
+                                    10,  // Shoulder
+                                    11}; // Elbow
---
+++ firmware/src/board_b/pinmap.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+#include <Arduino.h>
+
+constexpr uint8_t EN_PIN = 8;
+
+constexpr uint8_t STEP_PINS[3] = {2,  // Wrist roll
+                                  3,  // Wrist left
+                                  4}; // Wrist right
+
+constexpr uint8_t DIR_PINS[3] = {5,  // Wrist roll
+                                 6,  // Wrist left
+                                 7}; // Wrist right
+
+constexpr int8_t ENDSTOP_PINS[3] = {-1,  // Wrist roll (no endstop)
+                                    -1,  // Wrist left (no endstop)
+                                    -1}; // Wrist right (no endstop)
